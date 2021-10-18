5916a29d60caf5070af14db669f6924e36144cee
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-18 17:55:15 +0100

    Extract method

--- scripts/tests/link_munging_test.py
+++ scripts/tests/link_munging_test.py
@@ -13,12 +13,15 @@ class LinkMungingTestCase(unittest.TestCase):
     def test_github_link_with_description(self):
         v1 = r'[GitHub - UserName/sample-repo-name: Sample:,_()- Description](https://github.com/UserName/sample-repo-name)'
         v2 = r'[UserName/**sample-repo-name**](https://github.com/UserName/sample-repo-name): Sample:,_()- Description'
-        self.assertEqual(v2, convert_github_url(v1))  # add assertion here
+        self.check_conversion(v1, v2)
 
     def test_github_link_without_description(self):
         v1 = r'[GitHub - copperspice/doxypress](https://github.com/copperspice/doxypress)'
         v2 = r'[copperspice/**doxypress**](https://github.com/copperspice/doxypress)'
-        self.assertEqual(v2, convert_github_url(v1))  # add assertion here
+        self.check_conversion(v1, v2)
+
+    def check_conversion(self, v1, v2):
+        self.assertEqual(v2, convert_github_url(v1))
 
 
 if __name__ == '__main__':
