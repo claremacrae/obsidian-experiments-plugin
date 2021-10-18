ee953be10d1d7012a60e467b64a152258168bdc2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-18 17:48:50 +0100

    Give up on extended regex, as not pastable in to PyCharm and Obsidian plugin

--- scripts/tests/link_munging_test.py
+++ scripts/tests/link_munging_test.py
@@ -5,16 +5,7 @@ import re
 
 def convert_github_url(v1):
     # https://github.com/isiahmeadows/github-limits
-    github_repo_hyperlink = re.compile(r"""
-                        \[ # Open a markdown link
-                        GitHub\ -\                          # GitHub prefix to page title
-                        ([a-z\d)]+)                         # User/organisation name
-                        /
-                        ([a-z\d.-]+)                        # Repo name
-                        (:\ [A-Za-z\d :.\-,]+){0,1}         # Description of repo - optional
-                        ]
-                       \(https://github.com/\1/\2\)""",     # Repo URL
-                       re.VERBOSE)
+    github_repo_hyperlink = re.compile(r"""\[GitHub\ -\ ([a-z\d)]+)/([a-z\d.-]+)(:\ [A-Za-z\d :.\-,]+){0,1}]\(https://github.com/\1/\2\)""")
     return github_repo_hyperlink.sub(r'[\1/**\2**](https://github.com/\1/\2)\3', v1)
 
 
