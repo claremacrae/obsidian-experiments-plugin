cfec213e10b4bf368dbb30f7993d36525c006259
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-18 18:31:01 +0100

    Remove unnecessary . from repo name expression

--- scripts/tests/link_munging_test.py
+++ scripts/tests/link_munging_test.py
@@ -5,7 +5,7 @@ import re
 
 def convert_github_url(v1):
     # https://github.com/isiahmeadows/github-limits
-    github_repo_hyperlink = re.compile(r"""\[GitHub\ -\ ([A-Za-z\d)]+)/([a-z\d.-]+)(:\ [A-Za-z\d :.\-,_()]+){0,1}]\(https://github.com/\1/\2\)""")
+    github_repo_hyperlink = re.compile(r"""\[GitHub\ -\ ([A-Za-z\d)]+)/([a-z\d-]+)(:\ [A-Za-z\d :.\-,_()]+){0,1}]\(https://github.com/\1/\2\)""")
     return github_repo_hyperlink.sub(r'[\1/**\2**](https://github.com/\1/\2)\3', v1)
 
 
@@ -19,6 +19,13 @@ class LinkMungingTestCase(unittest.TestCase):
         v1 = r'[GitHub - copperspice/doxypress](https://github.com/copperspice/doxypress)'
         v2 = r'[copperspice/**doxypress**](https://github.com/copperspice/doxypress)'
         self.check_conversion(v1, v2)
+
+    # def test_github_failing_link(self):
+    #     # v1 = r'[GitHub - maskiran/lightroom_collection_tree: Mirror the collection/collection sets in Lightroom onto the file system](https://github.com/maskiran/lightroom_collection_tree)'
+    #     v1 = r'[GitHub - maskiran/lightroom_collection_tree: Mirror the collection](https://github.com/maskiran/lightroom_collection_tree)'
+    #     # v2 = r'[maskiran/**lightroom_collection_tree**](https://github.com/maskiran/lightroom_collection_tree): Mirror the collection/collection sets in Lightroom onto the file system'
+    #     v2 = r'[maskiran/**lightroom_collection_tree**](https://github.com/maskiran/lightroom_collection_tree): Mirror the collection'
+    #     self.check_conversion(v1, v2)
 
     def check_conversion(self, v1, v2):
         self.assertEqual(v2, convert_github_url(v1))
